b291f1cfa49cb7eb2040fd4c8fa7485d07b4d39a
nienkebos <nienkebos@live.com>
2016-10-27 15:17:45 +0200

    shallow render test

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -2,21 +2,26 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import chai, { expect } from 'chai'
 import chaiEnzyme from 'chai-enzyme'
+import spies from 'chai-spies'
+chai.use(spies)
 chai.use(chaiEnzyme())
 
 import { LoginContainer } from './Login'
 
 
 describe('<LoginContainer />', () => {
-  const wrapper = shallow(<LoginContainer />);
+  const wrapper = shallow(<LoginContainer />)
 
   it('renders a form', () => {
     expect(wrapper.find('form')).to.have.length(1)
   })
 
-  it('renders a form that contains a descendant with id password', () => {
+  it('renders a form that contains certain elements', () => {
+    const emailLabel = wrapper.childAt(1)
+
     expect(wrapper).to.have.tagName('form')
     expect(wrapper).to.have.descendants('#password')
+    expect(emailLabel.text()).to.equal('Email:')
   })
 
   it('has three input fields', () => {
